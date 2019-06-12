50b4c5b7d891b9597d42c9d0299dedf714c2f4e9
Manuel Palenzuela <manuelpalenzuelamerino@gmail.com>
2019-06-12 10:14:55 +0200

    Added easier color switching

--- config.h
+++ config.h
@@ -7,11 +7,20 @@ static const char *fonts[] = {
 	"Noto Sans Display Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#000000";
+static const char col_aqua[]        = "#00ffff";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                  fg          bg    */
+	[SchemeNorm] = { col_gray3,  col_gray1},
+	[SchemeSel] =  { col_gray4,  col_cyan },
+	[SchemeOut] =  { col_black,  col_aqua },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
